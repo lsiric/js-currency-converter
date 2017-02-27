e86b913cb1a883147d9c2df78fdf4a6a2e9350e9
lsiric <siric.luka@gmail.com>
2017-02-27 17:20:46 +0100

    added auto jsdoc upon build

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,9 @@ gulp.task('scripts', ['eslint'], function() {
 });
 
 gulp.task('build', ['scripts'], function () {
-	console.log(MODULE_NAME, 'built');
+	return exec('npm run jsdoc', function () {
+		console.log(MODULE_NAME, ' built!');
+	});
 });
 
 gulp.task('eslint', function () {
@@ -51,7 +53,7 @@ gulp.task('demo-build', ['build'], function () {
 });
 
 gulp.task('demo', ['demo-build'], function () {
-	exec(' http-server ./demo -p 8080 --cors -P \'http://free.currencyconverterapi.com/api/v3\' -o -c -1');
+	exec(' http-server ./demo -p 8080 --cors -P \'http://free.currencyconverterapi.com/api/v3\' -o -c-1');
 });
 
 gulp.task('default', ['build']);
